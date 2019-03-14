1449f34c5429b01ff1e96b69cd48cb18424499c4
Debashis Roy Bhowmik <debashis.buet08@gmail.com>
2019-03-14 19:45:47 +0600

    wsgi reverted for wagtail

--- core/wsgi.py
+++ core/wsgi.py
@@ -8,15 +8,10 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
-import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.dev")
 
-PROJECT_ROOT = os.path.dirname(os.path.dirname(
-    os.path.abspath(__file__)
-))
-sys.path.append(os.path.join(PROJECT_ROOT, "packages"))
 
 application = get_wsgi_application()
